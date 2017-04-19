f9a6d3a8885d224d224539d578b4fdbb7ce684e3
Alberto Corona <ac@albertocorona.com>
2017-04-19 17:39:48 -0500

    Remove old models
    Signed-off-by: Alberto Corona <ac@albertocorona.com>

--- src/models/page.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('page', {
-    url: DataTypes.STRING,
-    title: DataTypes.STRING,
-    content: DataTypes.TEXT,
-    rendered: DataTypes.TEXT,
-  })
-}
--- src/models/site.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('site', {
-    name: DataTypes.STRING,
-    initialized: DataTypes.BOOLEAN,
-  })
-}
